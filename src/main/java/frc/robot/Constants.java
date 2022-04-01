4f8f39c19735ad589a3dc411b82ad61ba22cc67f
ethanwuhoo <45618151+ethanwuhoo@users.noreply.github.com>
2022-03-31 17:31:19 -0700

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,24 +46,24 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 11; 
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 12; 
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 13; 
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(24.774); //FIXME
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(24.6917724609375); //FIXME
     
     // Front right drive ID, steer ID, encoder ID, offset
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 14;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 15;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 16;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(301.640); //FIXME
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(301.190185546875); //FIXME
 
     // Back left drive ID, steer ID, encoder ID, offset
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 17;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 18; 
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 19; 
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(347.08); //FIXME
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(45.16754150390625); //FIXME
     // Back right drive ID, steer ID, encoder ID, offset
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 20; 
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 21; 
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 22; 
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(78.39); //FIXME
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(77.8656005859375); //FIXME
 
     // Driveing Multipliers
     public static final double driveSlow = .4; //FIXME
@@ -90,8 +90,8 @@ public final class Constants {
 	public static final int ballOneSensorID = 1;
 	public static final int ballTwoSensorID = 2;
 	public static final double indexSpeedForward = -.3; //FIXME
-	public static final double indexSpeedSimple = .15; //FIXME
-    public static final double indexSpeedSlow = .25;
+	public static final double indexSpeedSimple = .3; //FIXME
+    public static final double indexSpeedSlow = .15;
     public static final double indexWrongBallOut = -.6;
     public static final double indexSpeedSimpleBack = -.2;
     public static final double readyIndexForShoot = .25;
