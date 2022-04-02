f2ec90ac0f3df8bee73f9dca695c957f8ea39af9
Spud <jeffrypig23@gmail.com>
2022-04-02 16:28:42 -0700

    Moved autonomous commands to their own package

--- src/main/java/frc/robot/commands/autonomous/AutoReadyIndex.java
+++ src/main/java/frc/robot/commands/autonomous/AutoReadyIndex.java
@@ -2,21 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
-public class AutoIntake extends CommandBase {
+public class AutoReadyIndex extends CommandBase {
 
-  IntakeSubsystem m_intakeSubsystem;
   IndexerSubsystem m_indexerSubsystem;
-  public AutoIntake(IntakeSubsystem m_intakeSubsystem, IndexerSubsystem m_indexerSubsystem) {
-    this.m_intakeSubsystem = m_intakeSubsystem;
+  IntakeSubsystem m_intakeSubsystem;
+  ShooterSubsystem m_shooterSubsystem;
+  public AutoReadyIndex(IndexerSubsystem m_indexerSubsystem, ShooterSubsystem m_shooterSubsystem) {
     this.m_indexerSubsystem = m_indexerSubsystem;
-    addRequirements(m_intakeSubsystem);
-    addRequirements(m_indexerSubsystem);
+    this.m_shooterSubsystem = m_shooterSubsystem;
   }
 
   // Called when the command is initially scheduled.
@@ -27,17 +27,14 @@ public class AutoIntake extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intakeSubsystem.intakeDown();
-    m_indexerSubsystem.indexBallSimple();
-    m_intakeSubsystem.intakeForward();
+    m_indexerSubsystem.indexBallSimpleBack();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_intakeSubsystem.intakeStop();
-    m_intakeSubsystem.intakeUp();
     m_indexerSubsystem.indexStop();
+    m_shooterSubsystem.shootHigh();
   }
 
   // Returns true when the command should end.
