a687b0a6f6a68049f4c69067b3973f90da10332a
ethanwuhoo <45618151+ethanwuhoo@users.noreply.github.com>
2022-04-04 11:03:00 -0700

    Compressor

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,13 +8,17 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
   CANSparkMax shooterMotor1, shooterMotor2;
+  Compressor compressor;
   public ShooterSubsystem() {
     shooterMotor1 = new CANSparkMax(Constants.shooterMotor1ID, MotorType.kBrushless);
     shooterMotor2 = new CANSparkMax(Constants.shooterMotor2ID, MotorType.kBrushless);
+    compressor = new Compressor(2, PneumaticsModuleType.REVPH);
     
   }
   public void shootLow() {
@@ -25,11 +29,13 @@ public class ShooterSubsystem extends SubsystemBase {
   public void shootHigh() {
     shooterMotor1.setVoltage(Constants.shootHigh);
     shooterMotor2.setVoltage(-Constants.shootHigh);
+    compressor.disable();
   }
 
   public void shootStop() {
     shooterMotor1.setVoltage(0);
     shooterMotor2.setVoltage(0);
+    compressor.enabled();
   }
 
   @Override
